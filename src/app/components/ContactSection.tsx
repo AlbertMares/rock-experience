cc402cd37935233466edf491657428a777ffaee8
AlbertMares <albertmaares@gmail.com>
2026-09-24 13:41:16 -0600

    style: Cambios en diseño

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -8,7 +8,7 @@ export default function ContactSection() {
     <section id="contacto" className="py-5 bg-gradient text-light" data-aos="fade-left">
       <Container>
         <Row className="justify-content-center">
-          <Col md={6} lg={5}> {/* más pequeño que antes */}
+          <Col md={6} lg={5}> 
             <h2 className="mb-4 fw-bold text-center">Contacto</h2>
             <ContactForm />
           </Col>
