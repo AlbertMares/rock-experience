576e84b5aec28c0fa296729589441fb94b754bdf
AlbertMares <albertmaares@gmail.com>
2026-09-24 09:29:10 -0600

    feat: Se agrega Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <Navbar bg="light" expand="lg" className="mb-4">
+      <Container>
+        <Navbar.Brand href="#inicio">ROCK EXPERIENCE</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#experiencias">Experiencias</Nav.Link>
+            <Nav.Link href="#beneficios">Beneficios</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+          </Nav>
+          <Button variant="primary" href="#contacto">Participar</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
