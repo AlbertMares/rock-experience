62c501d761bc342244622571738387e3076e5366
AlbertMares <albertmaares@gmail.com>
2026-09-24 09:17:59 -0600

    feat: Agregar bootstrap, metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+
+// Configuración de fuentes
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+// SEO Y METADATA
+export const metadata: Metadata = {
+  metadataBase: new URL("https://rock-experience.vercel.app"), //URL test {{Cambiar}}
+
+  title: "ROCK EXPERIENCE",
+  description:
+    "Descubre experiencias creadas para conectar marcas, tecnología y personas.",
+
+  openGraph: {
+    title: "ROCK EXPERIENCE",
+    description: "Vive algo diferente.",
+    url: "https://rock-experience.vercel.app", //URL test {{Cambiar}}
+    siteName: "ROCK EXPERIENCE",
+    images: [
+      {
+        url: "/hero.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Rock Experience Hero",
+      },
+    ],
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es" className={geistSans.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
